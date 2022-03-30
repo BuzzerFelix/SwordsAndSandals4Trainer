ab986730bca90638b41cd1cd9051e6d0a9f55d39
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-03-30 11:47:03 +0300

    Update Program.cs

--- Source Code/Program.cs
+++ Source Code/Program.cs
@@ -8,9 +8,6 @@ namespace SwordsAndSandals4Trainer
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
